25afbbadcbc0612434aedc6250c8a304fbd0060d
Diego Soares <diegoxtt1@gmail.com>
2022-07-11 04:18:17 -0300

    projeto finalizado

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,17 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+
+  const val DATABASE_NAME = "marvel.db"
+  const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+
+  const val PUBLIC_KEY = "c3fae2fae104b6c56075b159edda8f70"
+  const val PRIVATE_KEY = "65a30ec30402d4809aa4077187a6716364eafaa2"
+
+  const val LAST_SEARCH_QUERY = "last_search_query"
+  const val DEFAULT_QUERY = ""
+
+  const val APIKEY = "apikey"
+  const val HASH = "hash"
+  const val TS = "ts"
+}
